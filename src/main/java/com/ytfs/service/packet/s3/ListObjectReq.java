5e253460af36524fb1c167aa99d64a301d434d72
LAPTOP-IM0URKF0\phs <penghuaisong@sursen.net>
2019-05-16 16:31:13 +0800

    object list message

---
+++ src/main/java/com/ytfs/service/packet/s3/ListObjectReq.java
@@ -0,0 +1,13 @@
+package com.ytfs.service.packet.s3;
+
+public class ListObjectReq {
+    private String bucketName;
+
+    public String getBucketName() {
+        return bucketName;
+    }
+
+    public void setBucketName(String bucketName) {
+        this.bucketName = bucketName;
+    }
+}
---
+++ src/main/java/com/ytfs/service/packet/s3/ListObjectResp.java
@@ -0,0 +1,21 @@
+package com.ytfs.service.packet.s3;
+
+public class ListObjectResp {
+
+    private String[] names;
+
+    /**
+     * @return the names
+     */
+    public String[] getNames() {
+        return names;
+    }
+
+    /**
+     * @param names the names to set
+     */
+    public void setNames(String[] names) {
+        this.names = names;
+    }
+
+}
