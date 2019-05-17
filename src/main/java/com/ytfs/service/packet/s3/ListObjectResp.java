1fd373aaf84c6220e016846e2edfefe5632f2252
LAPTOP-IM0URKF0\phs <penghuaisong@sursen.net>
2019-05-17 17:06:23 +0800

    update list object msg

--- src/main/java/com/ytfs/service/packet/s3/ListObjectResp.java
+++ src/main/java/com/ytfs/service/packet/s3/ListObjectResp.java
@@ -5,19 +5,22 @@ import com.ytfs.common.SerializationUtil;
 import java.util.Map;
 
 public class ListObjectResp {
+    private SerializationUtil.MapObject<byte[]> mapObject;
 
-    Map<String,byte[]> map;
-    SerializationUtil.MapObject mapObject = new SerializationUtil.MapObject(map);
-
-    public Map<String, byte[]> getMap() {
-        return map;
+    public SerializationUtil.MapObject<byte[]> getMapObject() {
+        return mapObject;
     }
 
-    public void setMap(Map<String, byte[]> map) {
-        this.map = map;
+    public void setMapObject(SerializationUtil.MapObject<byte[]> mapObject) {
+        this.mapObject = mapObject;
     }
 
 
+    public void setMap(Map<String,byte[]> map) {
+        this.mapObject =new SerializationUtil.MapObject(map);
+    }
 
-
+    public Map<String,byte[]> getMap(){
+        return mapObject.toMap();
+    }
 }
