663bba06217b5391bd9cc87ece130d2c77e85a1e
liushepeng <liushepeng@sohu.com>
2019-05-20 17:30:11 +0800

    msg

--- src/test/java/com/ytfs/service/packet/TestMap.java
+++ src/test/java/com/ytfs/service/packet/TestMap.java
@@ -1,22 +1,25 @@
 package com.ytfs.service.packet;
 
 import com.ytfs.common.SerializationUtil;
-import java.util.ArrayList;
+import com.ytfs.service.packet.s3.ListObjectResp;
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 
 public class TestMap {
 
     public static void main(String[] args) {
+        ListObjectResp ss = new ListObjectResp();
 
         Map<String, byte[]> map = new HashMap();
-        map.put("sd", "value".getBytes());
-        map.put("sdq", "valque".getBytes());
-        byte[] bs = SerializationUtil.serializeMap(map);
+        //map.put("sd", "value".getBytes());
+        //map.put("sdq", "valque".getBytes());
 
-        Map<String, byte[]> map1 = SerializationUtil.deserializeMap(bs);
-        
+        ss.setMap(map);
+
+        byte[] bs = SerializationUtil.serialize(ss);
+
+        ListObjectResp sss = (ListObjectResp) SerializationUtil.deserialize(bs);
+        Map<String, byte[]> map1 = sss.getMap();
         System.out.print(map1.size());
 
     }
