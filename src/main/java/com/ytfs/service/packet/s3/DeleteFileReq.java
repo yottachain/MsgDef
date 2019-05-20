f6dfff4d82f017176680d5547fa8ee6ddd1a3968
LAPTOP-IM0URKF0\phs <penghuaisong@sursen.net>
2019-05-20 17:53:51 +0800

    delete object msg

---
+++ src/main/java/com/ytfs/service/packet/s3/DeleteFileReq.java
@@ -0,0 +1,62 @@
+package com.ytfs.service.packet.s3;
+
+import org.bson.types.ObjectId;
+
+public class DeleteFileReq {
+
+    private String bucketname;
+    private String fileName;
+    private ObjectId VNU;
+    private byte[] meta;
+
+    public byte[] getMeta() {
+        return meta;
+    }
+
+    public void setMeta(byte[] meta) {
+        this.meta = meta;
+    }
+
+    /**
+     * @return the bucketname
+     */
+    public String getBucketname() {
+        return bucketname;
+    }
+
+    /**
+     * @param bucketname the bucketname to set
+     */
+    public void setBucketname(String bucketname) {
+        this.bucketname = bucketname;
+    }
+
+    /**
+     * @return the fileName
+     */
+    public String getFileName() {
+        return fileName;
+    }
+
+    /**
+     * @param fileName the fileName to set
+     */
+    public void setFileName(String fileName) {
+        this.fileName = fileName;
+    }
+
+    /**
+     * @return the VNU
+     */
+    public ObjectId getVNU() {
+        return VNU;
+    }
+
+    /**
+     * @param VNU the VNU to set
+     */
+    public void setVNU(ObjectId VNU) {
+        this.VNU = VNU;
+    }
+
+}
