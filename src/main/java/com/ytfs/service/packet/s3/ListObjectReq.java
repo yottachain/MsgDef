d70c7c16c7edaa742cdb08ed04a85fd24527bf29
LAPTOP-IM0URKF0\phs <penghuaisong@sursen.net>
2019-05-27 17:41:14 +0800

    update list object msg

--- src/main/java/com/ytfs/service/packet/s3/ListObjectReq.java
+++ src/main/java/com/ytfs/service/packet/s3/ListObjectReq.java
@@ -1,7 +1,32 @@
 package com.ytfs.service.packet.s3;
 
+import org.bson.types.ObjectId;
+
+import java.util.Map;
+
 public class ListObjectReq {
     private String bucketName;
+
+    private ObjectId startId;
+
+    private Integer limit;
+
+
+    public Integer getLimit() {
+        return limit;
+    }
+
+    public void setLimit(Integer limit) {
+        this.limit = limit;
+    }
+
+    public ObjectId getStartId() {
+        return startId;
+    }
+
+    public void setStartId(ObjectId startId) {
+        this.startId = startId;
+    }
 
     public String getBucketName() {
         return bucketName;
--- src/main/java/com/ytfs/service/packet/s3/ListObjectResp.java
+++ src/main/java/com/ytfs/service/packet/s3/ListObjectResp.java
@@ -1,11 +1,23 @@
 package com.ytfs.service.packet.s3;
 
 import com.ytfs.common.SerializationUtil;
+import org.bson.types.ObjectId;
 
 import java.util.Map;
 
 public class ListObjectResp {
+    private ObjectId objectId;
+
     private SerializationUtil.MapObject<byte[]> mapObject;
+
+
+    public ObjectId getObjectId() {
+        return objectId;
+    }
+
+    public void setObjectId(ObjectId objectId) {
+        this.objectId = objectId;
+    }
 
     public SerializationUtil.MapObject<byte[]> getMapObject() {
         return mapObject;
