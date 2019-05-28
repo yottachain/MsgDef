2bf8df97774ba1302468a9030142016ce071d5d4
LAPTOP-IM0URKF0\phs <penghuaisong@sursen.net>
2019-05-28 14:55:50 +0800

    getObject

--- src/main/java/com/ytfs/service/packet/s3/GetObjectResp.java
+++ src/main/java/com/ytfs/service/packet/s3/GetObjectResp.java
@@ -1,8 +1,20 @@
 package com.ytfs.service.packet.s3;
 
+import org.bson.types.ObjectId;
+
 public class GetObjectResp {
 
     private String fileName;
+    private ObjectId objectId;
+
+
+    public ObjectId getObjectId() {
+        return objectId;
+    }
+
+    public void setObjectId(ObjectId objectId) {
+        this.objectId = objectId;
+    }
 
     public String getFileName() {
         return fileName;
