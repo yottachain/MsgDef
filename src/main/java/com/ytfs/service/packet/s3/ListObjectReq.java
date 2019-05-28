86307f37f98f10830b9c7c8c08705d9aba0b12ab
LAPTOP-IM0URKF0\phs <penghuaisong@sursen.net>
2019-05-28 19:14:54 +0800

    list object

--- src/main/java/com/ytfs/service/packet/s3/ListObjectReq.java
+++ src/main/java/com/ytfs/service/packet/s3/ListObjectReq.java
@@ -11,6 +11,15 @@ public class ListObjectReq {
 
     private Integer limit;
 
+    private String fileName;
+
+    public String getFileName() {
+        return fileName;
+    }
+
+    public void setFileName(String fileName) {
+        this.fileName = fileName;
+    }
 
     public Integer getLimit() {
         return limit;
